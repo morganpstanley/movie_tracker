d38cfdb92c853ff4170bddacbee658e25fe3b33a
morganpstanley <morganpstanley@gmail.com>
2019-09-20 13:49:50 -0600

    create database tables

--- db/migrate/02_create_movies.rb
+++ db/migrate/02_create_movies.rb
@@ -2,7 +2,10 @@ class CreateMovies < ActiveRecord::Migration
     def change
         create_table :movies do |t|
             t.string :name
+            t.string :director
+            t.string :genre
             t.string :user_id
+            t.integer :release_date
         end
     end
 end
