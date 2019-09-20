0ce4ca2d52164b38a9cefe3cf6403908d1a61d7b
morganpstanley <morganpstanley@gmail.com>
2019-09-20 13:20:58 -0600

    sets up database

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
-# Load the Rails application.
-require_relative 'application'
+ENV['SINATRA_ENV'] ||= "development"
 
-# Initialize the Rails application.
-Rails.application.initialize!
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require_all 'app'
